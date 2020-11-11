36e54c75b97eb236a66e9c6cc8f2b292e57901b9
Ivan Gonzalez <scratchmex@gmail.com>
2020-11-11 17:55:26 -0600

    feat(users): add Profile model

--- doorman/users/models.py
+++ doorman/users/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+# Create your models here
+class Profile(models.Model):
+    # - relational
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="profile", primary_key=True,
+    )
+    # - own
+    phone_number = PhoneNumberField()
+    mobile_number = PhoneNumberField()
